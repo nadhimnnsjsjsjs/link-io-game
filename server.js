703481001960608e86f351967093c3a9bce5bd38
nadhimnnsjsjsjs <79369505+nadhimnnsjsjsjs@users.noreply.github.com>
2026-05-08 10:19:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, './')));
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+// Serve all files in the current folder
+app.use(express.static(__dirname));
+
+// Route for the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let players = {};
 let orbs = [];
-let powerOrbs = []; 
+let powerOrbs = [];
 const WORLD_SIZE = 4000;
 
+// Setup World
 for (let i = 0; i < 70; i++) orbs.push({ id: i, x: Math.random() * WORLD_SIZE, y: Math.random() * WORLD_SIZE });
 for (let i = 0; i < 5; i++) powerOrbs.push({ id: i, x: Math.random() * WORLD_SIZE, y: Math.random() * WORLD_SIZE });
 
@@ -26,8 +34,11 @@ io.on('connection', (socket) => {
         players[socket.id] = {
             id: socket.id,
             name: userData.name || "Guest",
-            x: Math.random() * WORLD_SIZE, y: Math.random() * WORLD_SIZE,
-            trail: [], color: getUniqueColor(socket.id), score: 200
+            x: Math.random() * WORLD_SIZE,
+            y: Math.random() * WORLD_SIZE,
+            trail: [],
+            color: getUniqueColor(socket.id),
+            score: 200
         };
         socket.emit('init', { players, orbs, powerOrbs, myId: socket.id, worldSize: WORLD_SIZE });
         socket.broadcast.emit('newPlayer', players[socket.id]);
@@ -35,11 +46,7 @@ io.on('connection', (socket) => {
 
     socket.on('update', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-            players[socket.id].trail = data.trail;
-            players[socket.id].score = data.score;
-            // Broadcast to others less frequently or only on change
+            Object.assign(players[socket.id], data);
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
@@ -54,48 +61,35 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('eatPowerOrb', (id) => {
-        if (powerOrbs[id] && players[socket.id]) {
-            powerOrbs[id].x = Math.random() * WORLD_SIZE;
-            powerOrbs[id].y = Math.random() * WORLD_SIZE;
-            io.emit('updatePowerOrbs', powerOrbs);
-        }
-    });
-
     socket.on('slicePlayer', (data) => {
         const victim = players[data.victimId];
         const attacker = players[socket.id];
         if (victim && attacker && victim.trail.length > 0) {
             let slicePercent = (data.sliceIndex + 1) / victim.trail.length;
             let stolen = Math.floor(victim.score * slicePercent);
-            
             if (slicePercent > 0.6 || (victim.score - stolen) < 100) {
                 io.emit('explosion', { x: victim.x, y: victim.y, color: victim.color });
-                io.emit('killMessage', { killer: attacker.name, victim: victim.name });
                 attacker.score += victim.score;
                 victim.score = 200; victim.trail = [];
                 io.emit('syncScore', { playerId: victim.id, newScore: 200 });
                 io.emit('syncScore', { playerId: attacker.id, newScore: attacker.score });
                 io.to(victim.id).emit('forceDeath');
             } else {
-                victim.score -= stolen;
-                attacker.score += stolen;
+                victim.score -= stolen; attacker.score += stolen;
                 io.emit('syncScore', { playerId: victim.id, newScore: victim.score });
                 io.emit('syncScore', { playerId: attacker.id, newScore: attacker.score });
-                io.emit('doSlice', { victimId: data.victimId, sliceIndex: data.sliceIndex, x: victim.trail[data.sliceIndex].x, y: victim.trail[data.sliceIndex].y, stolen: stolen });
+                io.emit('doSlice', { victimId: data.victimId, sliceIndex: data.sliceIndex, x: victim.x, y: victim.y, stolen: stolen });
             }
         }
     });
 
-    socket.on('playerDied', (killerId) => {
-        if (players[socket.id]) {
-            players[socket.id].score = 200; players[socket.id].trail = [];
-            io.emit('syncScore', { playerId: socket.id, newScore: 200 });
-        }
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('removePlayer', socket.id);
     });
-
-    socket.on('disconnect', () => { delete players[socket.id]; io.emit('removePlayer', socket.id); });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, '0.0.0.0', () => { console.log(`Server running on port ${PORT}`); });
+http.listen(PORT, '0.0.0.0', () => {
+    console.log('Server is live on port ' + PORT);
+});
